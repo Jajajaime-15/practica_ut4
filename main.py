52ce172d2176818c94587d3d1fd9c4d76dace08c
Alba <barrosomartinalba@gmail.com>
2025-12-04 20:18:20 +0100

    empezando menu

--- main.py
+++ main.py
@@ -3,6 +3,41 @@ from peewee import * #type: ignore
 from playhouse import postgres_ext #type: ignore
 from playhouse import shortcuts #type: ignore 
 from database import db, inicializar_base
+from repositories.atraccion_repo import AtraccionRepo
+from repositories.ticket_repo import TicketRepo
+from repositories.visitante_repo import VisitanteRepo
 
 
 inicializar_base([])
+
+def principal():
+    while True:
+        print("--> MENU PARQUE DE ATRACCIONES <-- \n"\
+            "\n1. Visitantes\n"\
+            "\n2. Atracciones\n"\
+            "\n3. Tickets\n"\
+            "\n4. Operaciones JSON\n"\
+            "\n5. Consultas avanzadas\n"\
+            "\n0. Salir\n"
+            )
+        opcion = input("Elige una opcion: ")
+        match opcion:
+            case "1":
+                while True:
+                    print("--> VISITANTES\n" \
+                    "\n1. Crear visitante\n" \
+                    "\n2. Mostrar visitantes\n" \
+                    "\n3. Eliminar visitante\n" \
+                    "\n4. Mostrar visitantes con preferencia por las atracciones 'extremas'\n" \
+                    "n\5. Mostrar visitantes con problemas cardiacos\n" \
+                    "\n0. Volver\n"
+                    )
+                    opcion = input("Elige una opcion: ")
+                    match opcion:
+                        case "1":
+                            nombre = input("Nombre: ")
+                            email = input("Email: ")
+                            altura = input("Altura: ")
+                            preferencias = "" # MIRAR COMO SE METE JSON
+                            VisitanteRepo.crear_visitante(nombre, email, altura, preferencias)
+                            print("Visitante creado correctamente.")
